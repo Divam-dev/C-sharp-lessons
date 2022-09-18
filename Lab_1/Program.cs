5a6070593d78183622f66beffd2d996b24ed1dd0
Divam <masterpemoiseenko1@gmail.com>
2022-09-18 22:19:00 +0300

    Add 2 labs

---
+++ Lab_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Введіть a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введіть t: ");
+int t = int.Parse(Console.ReadLine()!);
+
+double S = (a * Math.Pow(t, 2)) / 2;
+Console.WriteLine($"Результат = {S}");
---
+++ Lab_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Введіть nk: ");
+int nk = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введіть nn: ");
+int nn = int.Parse(Console.ReadLine()!);
+
+if (nn <= 0 | nk <= nn) Console.WriteLine("Помилка введення");
+else
+{
+    double result = 1;
+    for (double k = nn; k < nk; k++)
+    {
+        double numerator = k * k - Math.Pow(-1, Math.Pow(k, 2) + 1) * k;
+        double denominator = k * k + 2;
+        result = numerator / denominator;
+
+    }
+    Console.WriteLine($"Результат = {result}");
+}
