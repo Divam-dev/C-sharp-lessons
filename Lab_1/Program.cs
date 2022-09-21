1e6200a1736e353fca869fa8c3294cae38c02d95
Divam <masterpemoiseenko1@gmail.com>
2022-09-21 21:59:37 +0300

    lab1,2

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,11 +1,39 @@
-﻿using System.Text;
-Console.OutputEncoding = Encoding.UTF8;
+﻿namespace ConsoleApp
+{
 
-Console.Write("Введіть a: ");
-int a = int.Parse(Console.ReadLine()!);
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-Console.Write("Введіть t: ");
-int t = int.Parse(Console.ReadLine()!);
+            double a, t;
+            bool isaOk, istOk;
 
-double S = (a * Math.Pow(t, 2)) / 2;
-Console.WriteLine($"Результат = {S}");
+            do
+            {
+                Console.Write("Введіть прискорення. a = ");
+                string stra = Console.ReadLine()!;
+                isaOk = double.TryParse(stra, out a);
+                if (!isaOk)
+                {
+                    Console.WriteLine("Помилка введення прискорення");
+                }
+            } while (!isaOk);
+
+            do
+            {
+                Console.Write("Введіть час. t = ");
+                string strt = Console.ReadLine()!;
+                istOk = double.TryParse(strt, out t);
+                if (!istOk)
+                {
+                    Console.WriteLine("Помилка введення часу");
+
+                }
+            } while (!istOk);
+
+            double S = (a * Math.Pow(t, 2)) / 2;
+            Console.WriteLine($"Результат = {S}");
+        }
+    }
+}
--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -1,22 +1,41 @@
-﻿using System.Text;
-Console.OutputEncoding = Encoding.UTF8;
-
-Console.Write("Введіть nk: ");
-int nk = int.Parse(Console.ReadLine()!);
-
-Console.Write("Введіть nn: ");
-int nn = int.Parse(Console.ReadLine()!);
-
-if (nn <= 0 | nk <= nn) Console.WriteLine("Помилка введення");
-else
+﻿namespace ConsoleApp
 {
-    double result = 1;
-    for (double k = nn; k < nk; k++)
+    class Program
     {
-        double numerator = k * k - Math.Pow(-1, Math.Pow(k, 2) + 1) * k;
-        double denominator = k * k + 2;
-        result = numerator / denominator;
+        static void Main(string[] args)
+        {
+            int nn, nk;
+            double result = 0;
+            bool isnnOk, isnkOk;
 
+            do
+            {
+                Console.Write("Введіть nk: ");
+                string strnk = Console.ReadLine()!;
+                isnkOk = int.TryParse(strnk, out nk);
+                if (!isnkOk || nk < 0)
+                {
+                    Console.WriteLine("Помилка введення nk");
+                }
+            } while (!isnkOk);
+
+            do
+            {
+                Console.Write("Введіть nn: ");
+                string strnn = Console.ReadLine()!;
+                isnnOk = int.TryParse(strnn, out nn);
+                if (!isnnOk && nn < 0)
+                {
+                    Console.WriteLine("Помилка введення nn");
+                }
+            } while (!isnnOk);
+            if (nn < 0 || nk < nn) Console.WriteLine("Помилка 0 <= nn <= nk");
+
+            for (int k = nn; k < nk; k++)
+            {
+                result = (k * k - Math.Pow(-1, Math.Pow(k, 2) + 1) * k) / (k * k + 2);
+            }
+            Console.WriteLine($"Результат = {result}");
+        }
     }
-    Console.WriteLine($"Результат = {result}");
 }
