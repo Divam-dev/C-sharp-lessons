63af75aec7989614a9e7f894d48eb01fa5c8bdf4
Divam <masterpemoiseenko1@gmail.com>
2022-10-04 23:23:41 +0300

    Update Program.cs

--- Lab_5/Program.cs
+++ Lab_5/Program.cs
@@ -4,16 +4,17 @@
     {
         static void Main(string[] args)
         {
-            const int m = 3;
+            const int m = 4;
             const int n = 3;
             int[,] arr = new int[m, n]{{ 4, 7, 3 },
                                        { 3, 6, 9 },
+                                       { 5, 2, 8 },
                                        { 0, 1, 4 }};
 
             Console.WriteLine("Початкова матриця:");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < m; j++)
+                for (int j = 0; j < n; j++)
                 {
                     Console.Write(arr[i, j] + " ");
                 }
@@ -25,11 +26,11 @@
 
             int k = 2;
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < m; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    if (i != k - 1 && j != k - 1)
+                    if (i != k - 1)
                     {
                         arr[i, j] = arr[i, j];
                         Console.Write(arr[i, j] + " ");
