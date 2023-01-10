cf33364c6a9174b69f1eaf0ea5c5266382e3a7e5
Divam <masterpemoiseenko1@gmail.com>
2023-01-10 15:19:06 +0200

    CarSorting

---
+++ CarSort/CarSort/Program.cs
@@ -0,0 +1,68 @@
+﻿using static Cars;
+
+class Cars
+{
+    public class Car
+    {
+        public string[] colors;
+        public int year;
+        public int price;
+        public string model;
+
+        public Car(string[] colors, int year, int price, string model)
+        {
+            this.colors = colors;
+            this.year = year;
+            this.price = price;
+            this.model = model;
+        }
+    }
+    public List<Car> cars = new List<Car>();
+
+    public Cars()
+    {
+        cars.Add(new Car(new string[] { "red", "black" }, 2018, 20000, "Honda Civic"));
+        cars.Add(new Car(new string[] { "white", "blue" }, 2020, 30000, "Toyota Corolla"));
+        cars.Add(new Car(new string[] { "black", "silver" }, 2016, 15000, "Mazda3"));
+        cars.Add(new Car(new string[] { "gray", "green" }, 2019, 25000, "Nissan Altima"));
+        cars.Add(new Car(new string[] { "silver", "brown" }, 2015, 10000, "Suzuki SX4"));
+    }
+
+    public Car FindCar(string color, int year, int price, string model)
+    {
+        foreach (Car car in cars)
+        {
+            if (car.colors.Contains(color) && car.year == year && car.price == price && car.model.Contains(model))
+            {
+                return car;
+            }
+        }
+        return null;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cars availableCars = new Cars();
+        Console.WriteLine("Enter color:");
+        string color = Console.ReadLine();
+        Console.WriteLine("Enter year:");
+        int year = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter price:");
+        int price = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter model:");
+        string model = Console.ReadLine();
+
+        Car chosenCar = availableCars.FindCar(color, year, price, model);
+
+        if (chosenCar == null)
+        {
+            Console.WriteLine("No car found with these parameters");
+        }
+        else
+        {
+            Console.WriteLine("Car with color: " + chosenCar.colors + " year: " + chosenCar.year + " price: " + chosenCar.price + " model: " + chosenCar.model);
+        }
+    }
+}
---
+++ SortingCars/SortingCars/Program.cs
@@ -0,0 +1,75 @@
+﻿class Cars
+{
+    public class Car
+    {
+        public List<string> Colors;
+        public int Year;
+        public int Price;
+        public string FuelType;
+        public string Model;
+    }
+
+    private List<Car> cars;
+
+    public Cars()
+    {
+        cars = new List<Car>
+        {
+            new Car {Colors = new List<string> {"white", "black"}, Year = 2020, Price = 20000, FuelType = "Gasoline", Model = "Honda Civic"},
+            new Car {Colors = new List<string> {"red", "blue"}, Year = 2019, Price = 25000, FuelType = "Diesel", Model = "Toyota Camry"},
+            new Car {Colors = new List<string> {"black", "silver"}, Year = 2021, Price = 30000, FuelType = "Gasoline", Model = "Mitsubishi Outlander"},
+            new Car {Colors = new List<string> {"gray", "green"}, Year = 2017, Price = 30000, FuelType = "Electric", Model = "Nissan Leaf"},
+            new Car {Colors = new List<string> {"white", "black", "silver"}, Year = 2016, Price = 25000, FuelType = "Gasoline", Model = "Mazda CX-5"}
+        };
+    }
+
+    public Cars.Car FindCar(List<string> colors, int year, int price, string fuelType)
+    {
+        Car result = cars.Find(car => colors.Any(color => car.Colors.Contains(color)) && car.Year == year && car.Price == price && car.FuelType == fuelType);
+        return result;
+    }
+
+    public void PrintCar(Cars.Car car)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        if (car == null)
+        {
+            Console.WriteLine("Немає автомобілів з заданими параметрами.");
+        }
+        else
+        {
+            Console.WriteLine("Модель: " + car.Model);
+            Console.WriteLine("Кольори: " + string.Join(", ", car.Colors));
+            Console.WriteLine("Рік: " + car.Year);
+            Console.WriteLine("Ціна: " + car.Price);
+            Console.WriteLine("Тип палива: " + car.FuelType);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Cars carInventory = new Cars();
+
+        Console.WriteLine("Введіть колір автомобіля:");
+        string colorInput = Console.ReadLine();
+        var colors = new List<string> { colorInput };
+
+        Console.WriteLine("Введіть рік випуску автомобіля:");
+        int year = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть ціну автомобіля:");
+        int price = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть тип палива автомобіля:");
+        string fuelType = Console.ReadLine();
+
+        Cars.Car selectedCar = carInventory.FindCar(colors, year, price, fuelType);
+        carInventory.PrintCar(selectedCar);
+    }
+}
